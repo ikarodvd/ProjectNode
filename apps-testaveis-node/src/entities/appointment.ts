877c76c5b24d1ef24be8dad1987dc348ad640444
ikarodvd <ikaromiguel13@gmail.com>
2023-04-29 18:18:19 -0400

    chore:tests unitarios basicos

---
+++ apps-testaveis-node/src/entities/appointment.ts
@@ -0,0 +1,27 @@
+export interface AppointmentProps {
+    customer: string;
+    startAt: Date;
+    endsAt: Date;
+}
+export class Appointment {
+    private props: AppointmentProps
+
+    get customer (){
+        return this.props.customer
+    }
+    get startAt (){ 
+        return this.props.startAt
+    }
+    get endsAt (){
+        return this.props.endsAt
+    }
+
+    constructor(props: AppointmentProps){
+        const {startAt, endsAt} = props
+        if(endsAt <= startAt){
+            throw new Error('Invalid end date')
+        }
+        this.props = props
+    }
+
+}
